4465f464130d309c2d4ef207f73ff5d1d226906e
Onur ÜRE <odtulufb@hotmail.com>
2025-05-28 13:17:07 +0300

    asd

--- App.tsx
+++ App.tsx
@@ -1,118 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {StyleSheet, View, Text, Button, Image} from 'react-native';
+import {useCheckVersion} from './useCheckVersion';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default function App() {
+  const {version} = useCheckVersion();
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <Text>asd </Text>
+      <Button
+        title={'check 11131313131313 OTA'}
+        onPress={version.onCheckVersion}
+      />
+      <Button
+        title={'check updateasdsadadadad11 OTA'}
+        onPress={version.onCheckVersion}
+      />
+      <Button title={'check update Git'} onPress={version.onCheckGitVersion} />
+      <Button title={'remove update Git'} onPress={version.removeGitUpdate} />
+      {version.state.loading && <Text>Loading from git...</Text>}
+      {!!version.state.progress && (
+        <View style={styles.progress}>
+          <View
+            style={[
+              styles.process,
+              {
+                width: `${version.state.progress}%`,
+              },
+            ]}
+          />
+        </View>
+      )}
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  progress: {
+    height: 10,
+    width: '80%',
+    marginTop: 20,
+    borderRadius: 8,
+    borderColor: 'grey',
+    borderWidth: 1,
+    overflow: 'hidden',
   },
-  highlight: {
-    fontWeight: '700',
+  process: {
+    height: 10,
+    backgroundColor: 'blue',
+  },
+  img: {
+    width: 180,
+    height: 180,
+    resizeMode: 'center',
+    marginBottom: 20,
   },
 });
-
-export default App;
--- ios/testap/AppDelegate.mm
+++ ios/testap/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
-#import "OtaHotUpdate.h"
+
 #import <React/RCTBundleURLProvider.h>
+#import "OtaHotUpdate.h"
 
 @implementation AppDelegate
 
@@ -16,11 +17,7 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-  #if DEBUG
-    return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
-  #else
-    return [OtaHotUpdate getBundle]; ## add this line
-  #endif
+  return [self bundleURL];
 }
 
 - (NSURL *)bundleURL
@@ -28,7 +25,7 @@
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [OtaHotUpdate getBundle];
 #endif
 }
 
